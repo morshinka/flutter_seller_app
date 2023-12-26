e1dc7f7f9277e77a8ac73c1feffc58057212c98e
Morshinka <tegarprarama461@gmail.com>
2023-12-26 20:16:34 +0700

    adding nofitication message

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,6 +1,6 @@
 class GlobarVariables {
   // harus dibuah sesuai link yang ada pada ngrok
   static const baseUrl =
-      'https://7c1c-2001-448a-406f-149b-b903-9318-d39f-9ffc.ngrok-free.app';
+      'https://8037-2001-448a-406f-149b-94a1-13f1-b512-6091.ngrok-free.app';
   // end ngrok
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_seller_app/bloc/Add_image/add_image_bloc.dart';
 import 'package:flutter_seller_app/bloc/Add_product/add_product_bloc.dart';
 import 'package:flutter_seller_app/bloc/Categories/categories_bloc.dart';
 import 'package:flutter_seller_app/bloc/Product/product_bloc.dart';
+import 'package:flutter_seller_app/data/datasource/firebase_messaging_remote_datasource.dart';
 import 'package:flutter_seller_app/pages/dashboard/seller_dashboard_page.dart';
 
 import 'bloc/login/login_bloc.dart';
 import 'bloc/logout/logout_bloc.dart';
 import 'bloc/register/register_bloc.dart';
 import 'data/datasource/auth_local_datasource.dart';
+import 'firebase_options.dart';
 import 'pages/auth/auth_pages.dart';
 import 'pages/utils/light_themes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseMessagingRemoteDatasource().initNotification();
   runApp(const MyApp());
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const SellerDashboardPage(),
-//     );
-//   }
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
